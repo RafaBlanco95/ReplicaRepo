b2ac7cf19626cec231f66fc241dd415db9e3c465
Rafael Blanco Aranda <rblancar@emeal.nttdata.com>
2023-05-04 00:16:43 +0200

    Project Created

---
+++ replica/src/main/java/com/salesianas/dam/replica/ReplicaFpApplication.java
@@ -0,0 +1,13 @@
+package com.salesianas.dam.replica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicaFpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicaFpApplication.class, args);
+	}
+
+}
